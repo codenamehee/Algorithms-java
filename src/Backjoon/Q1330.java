8a3d31df47ee0d0f5d52ce214d821afb02e078e2
codenamehee <jhyfordev@gmail.com>
2022-01-10 13:59:31 +0900

    Q1330 풀이:if문 사용

--- src/Backjoon/Q1330.java
+++ src/Backjoon/Q1330.java
@@ -1,2 +1,22 @@
-package Backjoon;public class Q1330 {
+package Backjoon;
+
+import java.util.Scanner;
+
+public class Q1330 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = Integer.parseInt(sc.next());
+        int b = Integer.parseInt(sc.next());
+        sc.close();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a == b) {
+            System.out.println("==");
+        } else if (a < b) {
+            System.out.println("<");
+        }
+    }
 }
