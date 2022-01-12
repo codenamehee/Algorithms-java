60d97831ff2e35e17db7793459acda884e5909a5
codenamehee <jhyfordev@gmail.com>
2022-01-12 15:01:35 +0900

    Q2753 3번째 풀이:중첩 if문에 대해 else를 처리해주지 않아서 계속 틀렸다가 고침

---
+++ src/Backjoon/Q2753.java
@@ -0,0 +1,26 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class Q2753 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        // 윤년 : 4의 배수 and 100의 배수가 아님, 또는 400의 배수
+        // year % 4 == 0 && year % 100 !=0
+        // year % 400 == 0
+        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                System.out.println("1");
+            } else if (year % 400 == 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        } else {
+            System.out.println("0");
+        }
+    }
+}
