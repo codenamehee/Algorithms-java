e5e91051f5097e3292b9c807a26310b90cae021c
codenamehee <jhyfordev@gmail.com>
2022-01-17 00:03:46 +0900

    Q2884 오답 수정

--- src/Backjoon/Q2884.java
+++ src/Backjoon/Q2884.java
@@ -13,11 +13,11 @@ public class Q2884 {
         // minute >= 0 && minute <= 45 이고
         // hour == 0 이면 hour = 11, minute = 60 - (45-minute) hour > 0 이면 hour = hour-1, minute
 
-        if (minute > 45 && minute <=59) {
-            hour = hour;
+        if (minute >= 45 && minute <=59) {
+//            hour = hour;
             minute = minute - 45;
             System.out.println(hour + " " + minute);
-        } else if (minute >=0 && minute <= 45) {
+        } else if (minute >=0 && minute < 45) {
             if (hour == 0) {
                 hour = 23;
                 minute = 60 - (45 - minute);
