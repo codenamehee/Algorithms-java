eb63c82f2f1b1e3c9eec80204b523dd16e083f31
codenamehee <jhyfordev@gmail.com>
2022-01-22 18:23:18 +0900

    Q2438 1번째 코드

--- src/Backjoon/Q2438.java
+++ src/Backjoon/Q2438.java
@@ -1,7 +1,24 @@
 package Backjoon;
 
-public class Q2438 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Q2438 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                bw.write('*');
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
