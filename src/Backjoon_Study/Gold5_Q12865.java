6d2b92cfa6a45f7c04810da1ebbf502428b00cbf
codenamehee <jhyfordev@gmail.com>
2022-04-29 18:10:58 +0900

    백준 스터디 Q12865 구글링 풀이

--- src/Backjoon_Study/Gold5_Q12865.java
+++
@@ -1,4 +0,0 @@
-package Backjoon_Study;
-
-public class Gold5_Q12865 {
-}
--- src/Backjoon_Study/Silver5_Q11650_Googled.java
+++ src/Backjoon_Study/Silver5_Q11650_Googled.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Silver5_Q11650 {
+public class Silver5_Q11650_Googled {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
