7e6aaf6b36cc32e708299787fe82fda08c1a3258
codenamehee <jhyfordev@gmail.com>
2022-05-02 15:15:42 +0900

    백준 스터디 Q9251 LCS 문제 풀이 정리

--- src/Backjoon_Study/Gold5_Q9251.java
+++ src/Backjoon_Study/Gold5_Q9251.java
@@ -1,7 +1,28 @@
 package Backjoon_Study;
 
-public class Gold5_Q9251 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Gold5_Q9251 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int[][] lcs = new int[str2.length() + 1][str1.length() + 1];
+
+        for (int i = 1; i <= str2.length(); i++) {
+            for (int j = 1; j <= str1.length(); j++) {
+                // lcs테이블에서 행과 열의 문자가 같을 때
+                if (str2.charAt(i-1) == str1.charAt(j-1)) {
+                    lcs[i][j] = lcs[i-1][j-1] + 1;
+                } else {
+                    // lcs테이블에서 행과 열의 문자가 다를 때
+                    lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+                }
+            }
+        }
+        System.out.println(lcs[str2.length()][str1.length()]);
     }
 }
